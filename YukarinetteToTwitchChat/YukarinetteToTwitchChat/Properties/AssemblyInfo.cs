251683bb90edc98d4d49fcd5d16af08a12f1ea42
WabisabiNeet <s037055@yahoo.co.jp>
2022-03-28 15:38:12 +0900

    Update AssemblyInfo.cs

--- YukarinetteToTwitchChat/YukarinetteToTwitchChat/Properties/AssemblyInfo.cs
+++ YukarinetteToTwitchChat/YukarinetteToTwitchChat/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("YukarinetteToTwitchChat")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ゆかりねっとの音声認識結果をTwitchチャットに投稿するプラグイン")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("YukarinetteToTwitchChat")]
